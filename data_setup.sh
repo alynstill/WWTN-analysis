f132aeb01ebddbc074ab5ef0317d4acb31948db2
Alyn Still <alynstill@users.noreply.github.com>
2018-08-31 11:00:30 +0100

    added note on url

--- data_setup.sh
+++ data_setup.sh
@@ -4,4 +4,5 @@
 #
 # download database setup script from the WWTN website
 # this will be saved locally to make it easier to compare & debug in the event of new releases
+# it's not clear whether this url is version-specific - so it should be checked against the links published on the WWTN website
 curl -o dbsetup.txt https://wwtn.history.qmul.ac.uk/database-static/101746.txt
