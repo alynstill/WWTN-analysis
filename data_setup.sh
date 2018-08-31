7e385bfa634947b5221433a4b0c230cb58bcd3c9
Alyn Still <alynstill@users.noreply.github.com>
2018-08-31 10:58:20 +0100

    Initial version

---
+++ data_setup.sh
@@ -0,0 +1,7 @@
+# WWTD data is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+# License details at - https://creativecommons.org/licenses/by-nc-sa/3.0/
+# Details of the Who Were The Nuns project at - https://wwtn.history.qmul.ac.uk/
+#
+# download database setup script from the WWTN website
+# this will be saved locally to make it easier to compare & debug in the event of new releases
+curl -o dbsetup.txt https://wwtn.history.qmul.ac.uk/database-static/101746.txt
